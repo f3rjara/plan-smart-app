6ecff16ba1d4c4610ad9702dc62793f6e3bc71a9
fernando.jaramillo <f3rjara@gmail.com>
2025-03-21 10:54:25 -0500

    refactor(app): :recycle: Actualizacion de  rutas en providers para app

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,8 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { routes } from './app.routes';
-import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeng/themes/aura';
-import { APP_PROVIDERS } from './app.providers';
+import { APP_PROVIDERS } from './configs/app.providers';
 
 /**
  * Configuración de la aplicación, Opciones de configuración para bootstrapApplication en main.ts. providers: Proveedores de servicios para la aplicación.
@@ -20,17 +17,6 @@ export const AppConfig: ApplicationConfig = {
     provideRouter(routes, withComponentInputBinding()),
     provideHttpClient(withInterceptorsFromDi()),
     provideAnimations(),
-    provideAnimationsAsync(),
-    providePrimeNG({
-      ripple: true,
-      theme: {
-        preset: Aura,
-        options: {
-          prefix: 'p',
-          darkModeSelector: '.dark',
-        },
-      },
-    }),
     ...APP_PROVIDERS,
   ],
 };
